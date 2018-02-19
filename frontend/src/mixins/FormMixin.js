b5d76a750016983247ca4ffb507cfd8dff0fe96e
Magnun Leno <magnun.leno@gmail.com>
2018-02-19 01:23:33 -0300

    Improves FormMixin

--- frontend/src/mixins/FormMixin.js
+++ frontend/src/mixins/FormMixin.js
@@ -1,7 +1,49 @@
 export const FormMixin = {
+  data () {
+    return {
+      defaultErrorMessage: 'Sorry, your form still have errors!',
+      networkErrorMessage: 'An error occured while communicating with the server, please try again later.',
+      nonFieldErrors: []
+    }
+  },
   methods: {
     isNotValidated () {
       return Object.values(this.fields).filter(i => i.required).some(i => !i.validated)
+    },
+    injectErrors (data) {
+      // Non Field Errors
+      if (data.non_field_errors) {
+        data.non_field_errors.forEach(value => {
+          this.nonFieldErrors.push(value)
+        })
+        delete data.non_field_errors
+      }
+
+      // API/HTTP Errors
+      if (data.detail) {
+        this.nonFieldErrors.push(data.detail)
+        delete data.detail
+      }
+
+      // Field Errors
+      Object.keys(data).forEach(name => {
+        if (this.fields.hasOwnProperty(name)) {
+          data[name].forEach(text => {
+            this.$validator.errors.add(name, text)
+          })
+        }
+      })
+    },
+    processErrors (err) {
+      if (err.ok === false && err.status === 0) {
+        this.nonFieldErrors.push(this.networkErrorMessage)
+      } else if (err.json) {
+        err.json().then(data => this.injectErrors(data))
+      } else if (typeof err === 'object') {
+        this.injectErrors(err)
+      } else {
+        this.nonFieldErrors.push(this.defaultErrorMessage)
+      }
     }
   }
 }
