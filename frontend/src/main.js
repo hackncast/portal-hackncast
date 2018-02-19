66796c4dd225e2b487f971ee8528f3f507f85e80
Magnun Leno <magnun.leno@gmail.com>
2018-02-19 01:16:56 -0300

    Adds VueResource and a small interceptor for CSRF token headers

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,13 +6,24 @@ import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
+import VueResource from 'vue-resource'
+
 import VueCookies from '@/plugins/VueCookies'
 import { store } from '@/store/store'
 
 Vue.use(Vuetify)
+Vue.use(VueResource)
 Vue.use(VueCookies)
 
 Vue.config.productionTip = false
+
+Vue.http.interceptors.push(function (request, next) {
+  let csrftoken = Vue.cookies.get('csrftoken')
+  if (request.method === 'POST' && csrftoken !== undefined) {
+    request.headers.set('X-CSRFToken', csrftoken)
+  }
+  next()
+})
 
 /* eslint-disable no-new */
 new Vue({
