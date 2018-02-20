a5064bbf1186e592c3edf1672b4d708ba751ef20
Magnun Leno <magnun.leno@gmail.com>
2018-02-20 17:12:37 -0300

    Componentizes the NavBar

---
+++ frontend/src/components/InsideNavBar.vue
@@ -0,0 +1,48 @@
+<template>
+<v-toolbar app dark dense fixed clipped-left complex color="blue-grey darken-4">
+  <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+    <v-toolbar-side-icon @click.stop=""></v-toolbar-side-icon>
+  </v-toolbar-title>
+  <v-spacer></v-spacer>
+  <v-toolbar-items class="">
+    <v-menu offset-y :close-on-content-click="false" :nudge-width="200" :nudge-bottom="5" :nudge-left="200" v-model="menu">
+      <v-btn flat dark slot="activator">
+        <v-icon>account_circle</v-icon>
+      </v-btn>
+      <v-card>
+        <v-list>
+          <v-list-tile avatar>
+            <v-list-tile-avatar>
+              <img :src="currentUser.avatar" :alt="currentUser.displayName">
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ currentUser.displayName }}</v-list-tile-title>
+              <v-list-tile-sub-title>{{ currentUser.email }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn flat @click="menu = false">Close</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="red darken-2" flat @click="">Logout <v-icon right>exit_to_app</v-icon></v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-menu>
+  </v-toolbar-items>
+</v-toolbar>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  data: () => ({
+    menu: false
+  }),
+
+  computed: {
+    ...mapGetters(['currentUser'])
+  }
+}
+</script>
--- frontend/src/pages/layouts/Inside.vue
+++ frontend/src/pages/layouts/Inside.vue
@@ -1,30 +1,24 @@
 <template>
   <v-app id="inspire">
-    <v-toolbar color="blue-grey darken-4" dark app :clipped-left="$vuetify.breakpoint.mdAndUp" fixed>
-      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
-        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <span class="text-md-left text-sm-center">Administration</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-    </v-toolbar>
-
+    <inside-nav-bar />
     <v-navigation-drawer dark class="blue-grey darken-2" fixed :clipped="$vuetify.breakpoint.mdAndUp" app :mini-variant.sync="mini"  value="true" v-model="drawer">
-        <v-list class="pa-0 blue-grey darken-3">
-          <v-list-tile avatar>
-            <v-list-tile-avatar>
-              <img :src="currentUser.avatar" >
-            </v-list-tile-avatar>
-            <v-list-tile-content>
-              <v-list-tile-title>{{ currentUser.displayName }}</v-list-tile-title>
-            </v-list-tile-content>
-            <v-list-tile-action>
-              <v-btn icon @click.native.stop="mini = !mini">
-                <v-icon>chevron_left</v-icon>
-              </v-btn>
-            </v-list-tile-action>
-          </v-list-tile>
-        </v-list>
-
+      <v-list class="pa-0 blue-grey darken-3">
+        <v-list-tile avatar>
+          <v-list-tile-avatar>
+            <img src="@/assets/hnc-logo-noframe-inverted.svg" alt="Logo" style="margin-top: -3px">
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>Portal Hack 'n' Cast</v-list-tile-title>
+            <v-list-tile-sub-title>Administration</v-list-tile-sub-title>
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-btn icon @click.native.stop="mini = !mini">
+              <v-icon>chevron_left</v-icon>
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+    
       <v-list class="pt-0" dense>
         <v-divider></v-divider>
         <v-list-tile v-for="item in items" :key="item.title" @click="">
@@ -35,19 +29,6 @@
             <v-list-tile-title>{{ item.title }}</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
-
-        <li>
-          <v-bottom-nav absolute :value="true" color="blue-grey darken-2" style="box-shadow: unset;">
-            <v-btn v-show="!mini">
-              <span>Settings</span>
-              <v-icon>settings</v-icon>
-            </v-btn>
-            <v-btn @click.native.stop="logout">
-              <span>Logout</span>
-              <v-icon>exit_to_app</v-icon>
-            </v-btn>
-          </v-bottom-nav>
-        </li>
       </v-list>
     </v-navigation-drawer>
 
@@ -65,7 +46,11 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import InsideNavBar from '@/components/InsideNavBar'
+
 export default {
+  components: {InsideNavBar},
+
   data: () => ({
     mini: true,
     dialog: false,
