008901390680cf4e39667c1d1f4f143ec461c0a6
Magnun Leno <magnun.leno@gmail.com>
2018-02-23 01:21:11 -0300

    Improves router transitions

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,27 @@
 <template>
-  <router-view/>
+  <div>
+  <transition :name="transitionName" mode="out-in">
+    <router-view/>
+  </transition>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+
+  data () {
+    return {
+      transitionName: ''
+    }
+  },
+
+  watch: {
+    '$route' (to, from) {
+      if (from.name !== null) {
+        this.transitionName = 'slide-fade'
+      }
+    }
+  }
 }
 </script>
