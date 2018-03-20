aa49e063ea068aafe40339e79aee25de14fc32b5
Magnun Leno <magnun.leno@gmail.com>
2018-03-20 17:10:54 -0300

    Adds zopfli webpack plugin for compressing assets

--- frontend/build/webpack.prod.conf.js
+++ frontend/build/webpack.prod.conf.js
@@ -10,6 +10,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const ZopfliPlugin = require("zopfli-webpack-plugin")
 
 const env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
@@ -119,7 +120,13 @@ const webpackConfig = merge(baseWebpackConfig, {
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
       }
-    ])
+    ]),
+
+    new ZopfliPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'zopfli',
+      test: /\.(js|html|css|jpe?g|gif|png|svg|ttf)$/
+    })
   ]
 })
 
