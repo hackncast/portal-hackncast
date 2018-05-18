66cbe7dd54eb37890c155beed92ea97ea2315b0b
Magnun Leno <magnun.leno@gmail.com>
2018-05-18 17:28:09 -0300

    Simplifies actions

--- frontend/src/store/modules/auth/actions.js
+++ frontend/src/store/modules/auth/actions.js
@@ -3,26 +3,18 @@ import UserSession from '@/models/UserSession'
 
 export default {
   fetchUserSessions ({ commit }) {
-    return new Promise((resolve, reject) => {
-      UserSession.service.all()
-        .then(data => {
-          commit(AUTH.SESSIONS, data)
-          resolve(data)
-        })
-        .catch(err => {
-          reject(err)
-        })
-    })
+    UserSession.service.all()
+      .then(data => {
+        commit(AUTH.SESSIONS, data)
+      })
+      .catch(err => { throw err })
   },
 
   removeUserSession ({ commit, state }, session) {
-    return new Promise((resolve, reject) => {
-      session.delete()
-        .then(data => {
-          commit(AUTH.REMOVE_SESSION, session)
-          resolve(data)
-        })
-        .catch(err => reject(err))
-    })
+    session.delete()
+      .then(data => {
+        commit(AUTH.REMOVE_SESSION, session)
+      })
+      .catch(err => { throw err })
   }
 }
