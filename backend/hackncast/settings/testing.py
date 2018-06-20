393b332af1b91af048c9f0236d7ca5e1e333896a
Magnun Leno <magnun.leno@gmail.com>
2018-06-20 01:43:10 -0300

    DummyCache for testing environment

--- backend/hackncast/settings/testing.py
+++ backend/hackncast/settings/testing.py
@@ -2,6 +2,10 @@ from .base import *
 
 DEFENDER_MOCK_REDIS = True
 
+CACHES['default'] = {
+    'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+}
+
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
