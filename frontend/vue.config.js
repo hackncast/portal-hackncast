6093def3d27814c82401ff4eae94cf5028d24588
Magnun Leno <magnun.leno@gmail.com>
2018-08-02 15:24:07 -0300

    Adds axios

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,16 @@
+const BACKEND = process.env.BACKEND ? process.env.BACKEND : 'http://localhost:8000'
+const proxyOpts = { target: BACKEND, ws: true, changeOrigin: true }
+
+console.log('PROXYING', proxyOpts)
+
 module.exports = {
+  baseUrl: '/admin/',
   devServer: {
-    disableHostCheck: true
+    disableHostCheck: true,
+    proxy: {
+      '/api': proxyOpts,
+      '/media': proxyOpts,
+      '/static': proxyOpts
+    }
   }
 }
