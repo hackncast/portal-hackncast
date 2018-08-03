1ca49e217983600f608e4b83923a1059d87674a1
Magnun Leno <magnun.leno@gmail.com>
2018-08-03 15:59:21 -0300

    Adds vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import routes from './routes'
 import store from './store'
 
-import plugins from './plugins'
-plugins.install(Vue)
+import '@babel/polyfill'
+
+// Styling
+import 'vuetify/dist/vuetify.min.css'
+import '@/style/base.sass'
+
+// Plugins
+import './plugins'
 
 Vue.config.productionTip = false
 
--- frontend/src/plugins/index.js
+++ frontend/src/plugins/index.js
@@ -1,3 +1,4 @@
+import Vue from 'vue'
 import axios from 'axios'
 import Vuetify from 'vuetify'
 import VueHead from 'vue-head'
@@ -5,12 +6,9 @@ import VueAxios from 'vue-axios'
 import VueMoment from 'vue-moment'
 import TopProgress from 'vue-top-progress'
 
-export default {
-  install (vue) {
-    vue.use(VueHead)
-    vue.use(Vuetify)
-    vue.use(VueMoment)
-    vue.use(TopProgress)
-    vue.use(VueAxios, axios)
-  }
-}
+Vue.use(VueHead)
+Vue.use(Vuetify)
+Vue.use(VueMoment)
+Vue.use(Vuetify, {})
+Vue.use(TopProgress)
+Vue.use(VueAxios, axios)
