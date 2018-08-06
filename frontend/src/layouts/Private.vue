6dac2da7d9e63aa2ea7549a3ac869e7d79d10115
Magnun Leno <magnun.leno@gmail.com>
2018-08-06 00:52:21 -0300

    Adds private layout

---
+++ frontend/src/layouts/Private.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app :dark="$store.state.ui.darkTheme" id="app">
+    <slot />
+  </v-app>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'PrivateLayout',
+
+  components: { },
+
+  computed: {
+    ...mapGetters({
+    })
+  }
+}
+</script>
